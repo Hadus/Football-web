287538440c670bfec3030748b1ad39068eab9647
kerian <18700872228@163.com>
2021-08-20 16:59:00 +0800

    temp

--- Login/login.js
+++ Login/login.js
@@ -36,6 +36,12 @@
     
     /* 方法：初始化 login */
     function init() {
+      bindBackGround(); // 绑定背景
+      // initLogin();
+    }
+
+    /* 方法：初始化 login */
+    function initLogin() {
       const s_username = d.querySelector('#s_username');
       const s_password = d.querySelector('#s_password');
       const s_login = d.querySelector('#s_login');
@@ -59,8 +65,6 @@
         const password = s_password.value;
         validLogin(username, password);
       })
-
-      bindBackGround(); // 绑定背景
     }
     
     /* 方法：登录校验 */
@@ -121,11 +125,87 @@
       }, .5 * 1000);
     }
 
+    /* 方法：切换背景图 */
     function bindBackGround() {
-      let backgroundImageUrl = '../lib/images/background_';
-      const index = '02';
-      backgroundImageUrl = backgroundImageUrl + index + '.jpeg';
-      d.body.style.backgroundImage = backgroundImageUrl;
+      const backgroundRefreshTime = 5;
+      const maxIndex = 2;
+      const backgroundImageUrlStart = '../lib/images/background_';
+
+      const sliderLeft = document.body.clientWidth;
+
+      const s_slider_box = d.querySelector('#s_slider_box');
+      const lisNode = [];
+      lisInit();
+      autoPlay();
+      
+      /* 方法：初始化 */
+      function lisInit() {
+        for(let i = 1; i < maxIndex + 1; i++) {
+          lisNode[i] = d.createElement('li');
+          i !== 1 && (lisNode[i].style.left = sliderLeft + 'px');
+          const img = d.createElement('img');
+          img.src = backgroundImageUrlStart + (i.toString().padStart(2, '0')) + '.jpeg';
+          lisNode[i].appendChild(img);
+          s_slider_box.appendChild(lisNode[i]);
+        }
+      }
+
+      /* 方法：初始化 */
+      function autoPlay() {
+        let index = 0;
+        // const timer = setInterval(() => {
+          
+        // }, backgroundRefreshTime * 1000);
+
+      }
+
+      /* 方法：初始化 */
+      function playOnce() {
+        animate(lis[key], {
+          left: -sliderLeft
+        });
+        key++;
+        key = key >= lis.length ? 0 : key;
+        lis[key].style.left = sliderLeft + "px"; //立马让下一张跑到右边去
+        setCtrl();
+        animate(lis[key], {
+          left: 0
+        });
+      }
+
+      /*  */
+      function animate(obj, json) {
+        clearInterval(obj.timer);
+        const stepTime = 30;
+        obj.timer = setInterval(function() {
+          var flag = true;
+          for (let attr in json) {
+            const current = parseInt(getCurrent(obj, attr));
+            const jsonAttr = parseInt(json[attr]);
+            let step = (jsonAttr - current) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            obj.style[attr] = current + step + "px";
+            //判断current和target是否相等
+            if (current != jsonAttr) {
+                flag = false;
+            }
+          }
+          if (flag) {
+            clearInterval(obj.timer);
+          }
+        }, stepTime);
+      }
+
+      /*  */
+      function getCurrent(obj, attr) {
+        if (window.getComputedStyle) {
+          return window.getComputedStyle(obj, null)[attr];
+        } else {
+          return obj.currentStyle[attr];
+        }
+      }
+      // ---- bindBackGround ---
     }
+
   }
 )(window, document);
